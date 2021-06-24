d565198c268785a9e9c28735ce1cb94d702f953e
SoRePeSi <sofia.rezende@edu.etefmc.com.br>
2021-06-24 11:43:40 -0300

    Adicionados comentários

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function aaa() {
+// Add delete button and checkbox to already existing elements on load
+// Adicionar botão de deletar e checkbox a elementos já-existentes ao carregar
+function start() {
     var itensOriginais = document.querySelectorAll("li");
     for (var i = 0; i < itensOriginais.length; i++) {
-        itensOriginais[i].style = "padding-left: 3%";
+        itensOriginais[i].style = "padding-left: 3%";   // Align with input box - Alinhar com caixa de entrada
         itensOriginais[i].appendChild(botaoDelete());
         itensOriginais[i].appendChild(checkBox());
     }
 }
 
+// Add new item to list
+// Adicionar novo item à lista
 function clique(event) {
     event.preventDefault();
 
@@ -26,12 +30,13 @@ function clique(event) {
 
 
         lista.appendChild(novoLi);
-        //document.getElementById("but").innerHTML = lista.length;
 
-        texto.value = "";
+        texto.value = "";   // Clear input box - Limpar caixa de entrada
     }
 }
 
+// Delete button (actually a span element)
+// Botão de deletar (na verdade um elemento span)
 function botaoDelete() {
     const botDel = document.createElement("span");
     botDel.style = "position: absolute; right: 5%;";
@@ -41,12 +46,16 @@ function botaoDelete() {
     return botDel;
 }
 
+// Delete item from list
+// Deletar item da lista
 function deleteLi(event) {
     var botClick = event.target;
     var delLi = botClick.parentElement;
     delLi.remove();
 }
 
+// Create checkbox element
+// Criar elemento checkbox
 function checkBox() {
     var box = document.createElement("input");
     box.className = "form-check-input";
@@ -57,6 +66,8 @@ function checkBox() {
     return box;
 }
 
+// Check or uncheck checkbox
+// Marcar ou desmarcar checkbox
 function marcarBox(event) {
     var checked = event.target;
     checked.value++;

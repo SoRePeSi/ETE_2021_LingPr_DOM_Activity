83769b1b4eebc100bb158e164c4eebd3e9e1eb57
SoRePeSi <sofia.rezende@edu.etefmc.com.br>
2021-06-24 10:50:58 -0300

    Strikethrough e função delete aperfeiçoados

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function aaa() {
-    document.querySelector("ul").style = "background-color:blue";
-
+    var itensOriginais = document.querySelectorAll("li");
+    for (var i = 0; i < itensOriginais.length; i++) {
+        itensOriginais[i].style = "padding-left: 3%";
+        itensOriginais[i].appendChild(botaoDelete());
+        itensOriginais[i].appendChild(checkBox());
+    }
 }
 
 function clique(event) {
@@ -12,11 +16,14 @@ function clique(event) {
     if (texto.value != "") {
         var novoLabel = document.createElement("label");
         novoLabel.innerHTML = "- " + texto.value;
+        novoLabel.className = "form-check-label";
         
         var novoLi = document.createElement("li");
         novoLi.appendChild(novoLabel);
         novoLi.appendChild(botaoDelete());
         novoLi.appendChild(checkBox());
+        novoLi.style = "padding-left: 3%";
+
 
         lista.appendChild(novoLi);
         //document.getElementById("but").innerHTML = lista.length;
@@ -44,6 +51,7 @@ function checkBox() {
     var box = document.createElement("input");
     box.className = "form-check-input";
     box.setAttribute("type", "checkbox");
+    box.value = 0;
     box.addEventListener("click", marcarBox);
 
     return box;
@@ -51,6 +59,12 @@ function checkBox() {
 
 function marcarBox(event) {
     var checked = event.target;
+    checked.value++;
     var checkedTask = checked.parentElement;
-    checkedTask.toggle("done");
+    if (checked.value % 2 != 0) {
+        checkedTask.style = "color:rgb(150,150,150); text-decoration:line-through; padding-left:3%";
+    }
+    else {
+        checkedTask.style = "color:black; padding-left:3%";
+    }
 }
